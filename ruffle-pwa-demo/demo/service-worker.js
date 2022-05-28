4a47af54218232488400eb787eebc6d9c38dca7a
nosamu <71368227+n0samu@users.noreply.github.com>
2022-05-28 18:08:46 -0500

    Add PWA demo

---
+++ ruffle-pwa-demo/demo/service-worker.js
@@ -0,0 +1,39 @@
+// Installing service worker
+const CACHE_NAME  = 'RuffleDemo';
+
+/* Add relative URL of all the static content you want to store in
+ * cache storage (this will help us use our app offline)*/
+let resourcesToCache = ["./", "./index.js", "./966.index.js", "../assets/logo.svg", "./logo-anim.swf"];
+
+self.addEventListener("install", e=>{
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache =>{
+            return cache.addAll(resourcesToCache);
+        })
+    );
+});
+
+// Cache and return requests
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    );
+});
+
+// Update a service worker
+const cacheWhitelist = ['RuffleDemo'];
+self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
