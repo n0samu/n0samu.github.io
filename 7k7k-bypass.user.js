9f8b5679c3ee293d865fefd7b2750c33511de154
nosamu <71368227+n0samu@users.noreply.github.com>
2022-10-23 05:19:02 -0500

    Add 7k7k login bypass userscript

---
+++ 7k7k-bypass.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        7k7k Login Bypass
+// @namespace   n0samu.github.io
+// @include     http://www.7k7k.com/*
+// @grant       none
+// @version     1.0
+// @author      nosamu
+// @description Play games on 7k7k.com without logging in!
+// @run-at document-idle
+// ==/UserScript==
+
+document.querySelector(".fixedModal").remove();
+play22.playLoading();
